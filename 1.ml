78a24658b9bec5a6a179b18c4099fca52d477c3e
Colin Jones <trptcolin@gmail.com>
2012-09-30 14:21:49 -0500

    Starting to learn a little ML

---
+++ 1.ml
@@ -0,0 +1,24 @@
+(*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+*)
+
+use "lazy_utils.ml";
+
+fun sum(xs) = foldl op + 0 xs;
+
+fun divides a b = (a mod b = 0);
+
+fun dividesEither(a, b) =
+  sfilter (fn x => (divides x a orelse divides x b));
+
+(* Note: currently paying the price of holding all the (divisible) numbers in
+ * memory, since there's not a fold that works on streams *)
+fun upto(limit) = takeWhile (fn x => x < limit);
+
+fun answer(a, b, limit) =
+  sum (upto(1000) (dividesEither(3, 5) naturalNumbers));
+
+answer(3, 5, 1000) = 233168;
---
+++ lazy_utils.ml
@@ -0,0 +1,43 @@
+(* most lazy details are from Programming in Standard ML, Robert Harper
+ * notable exception: Compiler.Control.Lazy.enabled has moved to Control.lazysml *)
+Control.lazysml := true;
+open Lazy;
+datatype lazy 'a stream = Cons of 'a * 'a stream;
+
+fun shd(Cons(h,_)) = h;
+fun stl(Cons(_,t)) = t;
+fun lazy lstl(Cons(_,t)) = t;
+
+fun smap f =
+  let
+    fun lazy loop (Cons(x, s)) =
+      Cons(f x, loop s)
+  in
+    loop
+  end;
+
+val rec lazy naturalNumbers = Cons(1, smap (fn x => x+1) naturalNumbers);
+
+fun sfilter f =
+  let
+    fun lazy loop (Cons(x, s)) =
+      if f x then Cons(x, loop s)
+      else loop s
+  in
+    loop
+  end;
+
+fun take 0 _ : 'a list = nil
+  | take n (Cons(x,s)) = x :: take (n-1) s;
+
+fun takeWhile f (Cons(x,xs)): 'a list =
+  if f x then x :: takeWhile f xs
+  else nil;
+
+fun drop 0 s : 'a stream = s
+  | drop n (Cons(x,s)) = drop (n-1) s;
+
+fun dropWhile f (Cons(x,xs)): 'a stream =
+  if f x then dropWhile f xs
+  else Cons(x, xs);
+
